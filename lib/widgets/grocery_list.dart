50b12bfcdd48d552fe4f9d3fa04c5018db0473ed
hoosamMr <hoosam.mr@gmail.com>
2023-12-15 17:37:45 +0200

    first commit

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GroceryList extends StatelessWidget{
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
