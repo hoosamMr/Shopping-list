9578ca8a415109943e69b6058d974a9e3b3cd9fc
hoosamMr <hoosam.mr@gmail.com>
2023-12-15 18:28:34 +0200

    adding ListView.builder->ListTile

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget{
+class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Your Groceries',
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text('${groceryItems[index].quantity}'),
+        ),
+      ),
+    );
   }
 }
