39e1cb60af570fa38b7b99bc8a4577e8c35a4ff7
hoosamMr <hoosam.mr@gmail.com>
2023-12-26 22:32:41 +0200

    adding firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+//import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,6 +15,38 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItem extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selctedCategory = categories[Categories.vegetables]!;
+  void _saveItem() async{
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      final url = Uri.https(
+        'flutter-perp-d8ae6-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selctedCategory.title,
+          },
+        ),
+      );
+      // print(response.body);
+      // print(response.statusCode);
+      
+      if(!context.mounted)return;
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +56,7 @@ class _NewItem extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -28,9 +64,19 @@ class _NewItem extends State<NewItem> {
                 decoration: const InputDecoration(
                   label: Text('Name'),
                   hintText: 'name',
+                  //labelStyle: TextStyle(color: Colors.white)
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be btween 1 and 50 characters.';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
                 },
               ) //instead of TextField widget
               //,const Divider() //SizedBox(height: 6,child: Placeholder(color: Colors.white,),)
@@ -38,19 +84,35 @@ class _NewItem extends State<NewItem> {
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                 Expanded(child:  TextFormField(
-                    decoration: const InputDecoration(
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
                         //alignLabelWithHint: true,
-                        label: Text('Quantity')),
-                    initialValue: '1',
-                    keyboardType: TextInputType.number,
-                  ),),
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '$_enteredQuantity',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid paseteve number.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
                   const SizedBox(
                     width: 8,
                   ),
                   Expanded(
                     child: DropdownButtonFormField(
-                      hint: const Text('color'),
+                      value: _selctedCategory,
+                      //hint: const Text('Category'),
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -70,9 +132,31 @@ class _NewItem extends State<NewItem> {
                             ),
                           ),
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          _selctedCategory = value!;
+                        });
+                      },
                     ),
                   )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end, //optional
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
+                  ),
                 ],
               )
             ],
